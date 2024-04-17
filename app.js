6e728bde60013c5dd01a5b485c5c9f3fd7859f23
Sushil kumar <isushilchaudhary@gmail.com>
2024-04-18 01:43:36 +0530

    Exchange rate buuton function

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const BASE_URL =
-  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies";
 
 const dropdownSelect = document.querySelectorAll(".dropdown select");
+const buttonVal = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const message = document.querySelector(".msg");
 // console.log(dropdownSelect);
 
 // for (code in countryList) {
@@ -33,3 +37,32 @@ const updateFlag = (element) => {
   let img = element.parentElement.querySelector("img");
   img.src = newSrc;
 };
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amountValue = amount.value;
+  if (amountValue == "" || amountValue < 1) {
+    amountValue = 1;
+    amount.value = 1;
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let toCurrVal = toCurr.value.toLowerCase();
+  let fromCurrVal = fromCurr.value.toLowerCase();
+  let rate = data[fromCurrVal][toCurrVal];
+
+  let finalAmount = rate * amountValue;
+  message.innerText = `${amountValue}  ${
+    fromCurr.value
+  } = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+};
+
+buttonVal.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  updateExchangeRate();
+});
